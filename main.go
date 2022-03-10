2b279c39885bb23b4e418fd8cfa34bd7f32e145f
Besar <limanibesar@gmail.com>
2022-03-10 16:46:05 -0500

    task 2 done

--- main.go
+++ main.go
@@ -25,10 +25,33 @@ func main() {
 	originUnit = strings.ToUpper(os.Args[1])
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
-
+		_, err := fmt.Scanln(&originValue)
+		if err != nil {
+			printError(errReadingInput)
+		}
+		if originUnit == "C" {
+			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
+		}
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+		_, err = fmt.Scanln(&shouldConvertAgain)
+		if err != nil {
+			printError(errReadingInput)
+		}
 
-		if shouldConvertAgain != "Y" {
+		/*
+			Currently, we can't always be sure that the value assigned to shouldConvertAgain
+			is going to be in the casing we expect. Let's fix this by making a small change to
+			the condition on the last if statement inside the for loop. The condition
+			should be a combination of calling the strings.ToUpper() function,
+			passing it the result of calling the strings.TrimSpace()
+			function with shouldConvertAgain as its argument.
+			If the result of all of that is NOT equal to "Y", then
+			the existing if block containing fmt.Println("Good bye!")
+			followed by break should be run.*/
+
+		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
