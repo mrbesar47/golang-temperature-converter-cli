c6438549d0c895e97bf953737b19d9a160c27321
Besar <limanibesar@gmail.com>
2022-03-10 16:46:44 -0500

    task 2 done - deleted comment

--- main.go
+++ main.go
@@ -40,17 +40,6 @@ func main() {
 			printError(errReadingInput)
 		}
 
-		/*
-			Currently, we can't always be sure that the value assigned to shouldConvertAgain
-			is going to be in the casing we expect. Let's fix this by making a small change to
-			the condition on the last if statement inside the for loop. The condition
-			should be a combination of calling the strings.ToUpper() function,
-			passing it the result of calling the strings.TrimSpace()
-			function with shouldConvertAgain as its argument.
-			If the result of all of that is NOT equal to "Y", then
-			the existing if block containing fmt.Println("Good bye!")
-			followed by break should be run.*/
-
 		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
